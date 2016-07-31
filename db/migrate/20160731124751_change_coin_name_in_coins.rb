6265b855db972d04996fecaba2fb5f8f31714cc9
libovness <jonathan.libov@gmail.com>
2016-07-31 09:01:02 -0400

    fixed migration for pg

--- db/migrate/20160731124751_change_coin_name_in_coins.rb
+++ db/migrate/20160731124751_change_coin_name_in_coins.rb
@@ -1,6 +1,5 @@
 class ChangeCoinNameInCoins < ActiveRecord::Migration[5.0]
   def change
-  	add_index :coins, :name
-  	change_column :coins, :name, :unique
+  	add_index :coins, :name, unique: true
   end
 end
