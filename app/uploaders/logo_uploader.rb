02a78831c443af2f31de79a4ba506093b9bb76b8
libovness <jonathan.libov@gmail.com>
2016-08-30 17:30:59 -0400

    added fallback image

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -22,9 +22,7 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-   ActionController::Base.helpers.asset_path("fallback/" + [version_name, "no-coin-image.png"].compact.join('_'))
-   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "/assets/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
   include CarrierWave::MiniMagick
--- app/uploaders/network_logo_uploader.rb
+++ app/uploaders/network_logo_uploader.rb
@@ -26,6 +26,10 @@ class NetworkLogoUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [60,60]
   end
 
+  def default_url
+    "/assets/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
