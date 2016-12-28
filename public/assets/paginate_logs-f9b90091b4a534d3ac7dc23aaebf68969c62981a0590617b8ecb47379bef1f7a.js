e91e01a7d1e2fc6f0b7a77c1cd6d9f5f5c7e5b85
libovness <jonathan.libov@gmail.com>
2016-12-28 16:09:57 -0500

    precompiled assets

---
+++ public/assets/paginate_logs-f9b90091b4a534d3ac7dc23aaebf68969c62981a0590617b8ecb47379bef1f7a.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  prev_scroll_top = 0;
+  $(window).scroll(function() {
+    if(window.pagination_loading) {
+      return;
+    }
+    scroll_height = $('body').prop('scrollHeight');
+    scroll_top = $('body').scrollTop();
+    body_height = $('body').height();
+    remaining = scroll_height - (scroll_top + body_height);
+    if (remaining < 40) {
+      curr_bottom = $('.action-links').css('bottom');
+      curr_bottom = 40 + (40 - remaining);
+      $('.action-links').css('bottom', curr_bottom);
+    } else {
+      $('.action-links').css('bottom',15);
+    };
+    if ($('.pagination').length) {
+      var url = $('.pagination a[rel=next]').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 100) {
+        window.pagination_loading = true;
+        $('.loading-more').show();
+        $.getScript(url).always(function() {
+          window.pagination_loading = false;
+        });
+      }
+    }
+  });
+});
---
+++ public/assets/show_alerts-83d43e54a5b73131a3228af570aa83c86ff19089ada8505864732ea203f8f2d1.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	
+	if ($('p.alert').text() != '') {
+		$('p.alert').show();
+	}
+	if ($('p.notice').text() != '') {
+		$('p.notice').show();
+	}
+	
+	text = $('#header-link-account').find('p').text();
+	$('#header-link-account').hover(
+		function() {
+			$(this).find("a.user-activity > p").hide();
+			$(this).find("a.header-link-account-glyphicon").show();
+		}, function() {
+			$(this).find("a.user-activity > p").show();
+			$(this).find("a.header-link-account-glyphicon").hide();
+		});
+});
