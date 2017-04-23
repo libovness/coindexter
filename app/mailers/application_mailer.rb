7ebe546f90af29222320aaba5f1ddd352d942551
libovness <jonathan.libov@gmail.com>
2017-04-23 08:14:03 -0400

    changed user emails to HTML

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'do-not-reply@coindexter.io'
+  default from: 'jonathan@coindexter.io'
   layout 'mailer'
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'jonathan.coindexter@gmail.com'
+  config.mailer_sender = 'jonathan@coindexter.io'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'UserMailer'
