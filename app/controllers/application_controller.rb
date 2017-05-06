cddce32e83ccceac2cdd546cbdbee64b37a5b30d
libovness <jonathan.libov@gmail.com>
2017-05-06 18:27:15 -0400

    fixed some style things with notices and fixed redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    super resource
   end
 
   private
