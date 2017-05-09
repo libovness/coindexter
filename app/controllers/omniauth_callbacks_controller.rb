dec5f591b2464f4aecd460c39bab05de67e802d6
libovness <jonathan.libov@gmail.com>
2017-05-09 17:22:19 -0400

    tried to fix omniauth 4

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -46,29 +46,46 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         puts "tannehill 12" 
       end
       puts "tannehill 13"
+      @user.skip_confirmation!
       @identity.update_attribute( :user_id, @user.id )
     end
 
     if @user.persisted?
       puts "tannehill 14"
       @identity.update_attribute( :user_id, @user.id )
+      puts "tannehill 15"
       # This is because we've created the user manually, and Device expects a
       # FormUser class (with the validations)
+      puts "tannehill 16"
       @user = FormUser.find @user.id
+      puts "tannehill 17"
       if @user.email.blank? && @identity.email
+        puts "tannehill 18"
         @user.update_attribute( :email, @identity.email)
+        puts "tannehill 19"
       end
       if @user.first_name.blank?
+        puts "tannehill 20"
         @user.update_attributes( :first_name => first_name(@identity.name), :last_name => last_name(@identity.name))
+        puts "tannehill 21"
       end
       if @user.remote_avatar_url.nil?
+        puts "tannehill 22"
         @user.update_attribute( :remote_avatar_url, @identity.image)
+        puts "tannehill 23"
       end
+      puts "tannehill 24"
       sign_in_and_redirect @user, event: :authentication
+      puts "tannehill 25"
       set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
     else
+      puts "tannehill 26"
       session["devise.#{provider}_data"] = env["omniauth.auth"]
+      puts "tannehill 27"
+      @user.skip_confirmation!
+      puts "tannehill 28"
       redirect_to new_user_registration_url
+      puts "tannehill 29"
     end
   end
 
