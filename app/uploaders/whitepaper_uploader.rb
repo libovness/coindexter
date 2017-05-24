98017cf2afb7a41c1c319d4f954b8ac696cd24e4
libovness <jonathan.libov@gmail.com>
2017-05-24 19:26:03 -0400

    partially fixed whitepaper uploader

--- app/uploaders/whitepaper_uploader.rb
+++ app/uploaders/whitepaper_uploader.rb
@@ -5,7 +5,6 @@ class WhitepaperUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  # include CarrierWaveDirect::Uploader
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
