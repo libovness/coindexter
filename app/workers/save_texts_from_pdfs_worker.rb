d3eb0e8fdd55f12289682da5d03944c2a9621e26
libovness <jonathan.libov@gmail.com>
2017-05-25 19:58:59 -0400

    turning jobs back on

--- app/workers/save_texts_from_pdfs_worker.rb
+++ app/workers/save_texts_from_pdfs_worker.rb
@@ -2,7 +2,6 @@ class SaveTextsFromPdfsWorker
   include Sidekiq::Worker
 
   def perform
-=begin
     Whitepaper.all.each do |wp|
 		if wp.fulltext.nil?
 			io = open('https://s3.amazonaws.com/coindexter/' + wp.whitepaper.current_path)
@@ -14,8 +13,6 @@ class SaveTextsFromPdfsWorker
 			wp.save
 		end
 	end
-	puts 'hey'
-=end
   end
 
   SaveTextsFromPdfsWorker.perform_async
--- app/workers/update_all_coin_prices_worker.rb
+++ app/workers/update_all_coin_prices_worker.rb
@@ -2,12 +2,9 @@ class UpdateAllCoinPricesWorker
   include Sidekiq::Worker
 
   def perform
-=begin
     Coin.all.each do |coin|
     	coin.update_prices
     end
-    puts 'hey'
-=end
   end
 
   UpdateAllCoinPricesWorker.perform_async
