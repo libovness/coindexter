463c7581d9ac55d2cff7d8eaf212a9c35f841891
libovness <jonathan.libov@gmail.com>
2017-05-28 15:38:02 -0400

    trying to fix sitemap generator

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-	config.cache_dir = "#{Rails.root}/tmp/"
-	config.permissions = 0666
+	# config.cache_dir = "#{Rails.root}/tmp/"
+	# config.permissions = 0666
 	config.fog_credentials = {
 	# Configuration for Amazon S3
 	:provider => 'AWS', :aws_access_key_id => ENV['S3_ACCESS_KEY'], :aws_secret_access_key => ENV['S3_SECRET_KEY']
