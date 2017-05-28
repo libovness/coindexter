53493b5cdd97da3e66bcc8a9fdf25d4c492cd18b
libovness <jonathan.libov@gmail.com>
2017-05-28 08:35:54 -0400

    added site metadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,40 @@
 module ApplicationHelper
 
-	def full_title(page_title = '')
-	    base_title = "Coindexter"
-	    if page_title.empty?
-			base_title
-		else
-	      page_title + " | " + base_title
-	    end
-	end
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Coindexter"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def page_description(page_description)
+    base_description = "Coindexter is a way to research blockchain networks and app coins."
+    if page_description.empty?
+      base_description
+    else
+      "#{page_description}"
+    end
+  end
+
+  def canonical_url(url)
+    base_url = "https://coindexter.io"
+    if url.empty?
+      base_url
+    else
+      "#{url}"
+    end
+  end
+
+  def og_image(og_image)
+    base_og_image = "apple-icon.png"
+    if og_image.empty?
+      base_og_image
+    else
+      "#{og_image}"
+    end
+  end  
 
 end
