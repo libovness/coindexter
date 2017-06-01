776e7be454cd04f89651df0914fa850781169fdb
libovness <jonathan.libov@gmail.com>
2017-06-01 12:54:26 -0400

    debugging worker 4

--- app/workers/update_all_coin_prices_worker.rb
+++ app/workers/update_all_coin_prices_worker.rb
@@ -10,12 +10,12 @@ class UpdateAllCoinPricesWorker
 		    if response[0].nil?
 		      puts coin.name
 		    else
-		      coin.price = response[0]["price_usd"]
-		      coin.one_hour_price_change = response[0]["percent_change_1h"]
-		      coin.one_day_price_change = response[0]["percent_change_24h"]
-		      coin.available_supply = response[0]["available_supply"]
-		      coin.total_supply = response[0]["total_supply"]
-		      coin.market_cap = response[0]["market_cap_usd"]
+		      price = response[0]["price_usd"]
+		      one_hour_price_change = response[0]["percent_change_1h"]
+		      one_day_price_change = response[0]["percent_change_24h"]
+		      available_supply = response[0]["available_supply"]
+		      total_supply = response[0]["total_supply"]
+		      market_cap = response[0]["market_cap_usd"]
 		      coin.update_attributes(:price => price,:one_day_price_change => one_day_price_change, :one_hour_price_change => one_hour_price_change, :available_supply => available_supply, :total_supply => total_supply, :market_cap => market_cap)
 		      coin.save
 		    end
