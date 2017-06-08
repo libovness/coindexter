4c9dca561fdbf0bd16a95aef80e4a6b26833fe6b
libovness <jonathan.libov@gmail.com>
2017-06-07 22:53:35 -0400

    trying to fix whitepaper scrape

--- app/workers/save_texts_from_pdfs_worker.rb
+++ app/workers/save_texts_from_pdfs_worker.rb
@@ -7,7 +7,7 @@ class SaveTextsFromPdfsWorker
   def perform
     Whitepaper.find_each do |wp|
 		if wp.fulltext.nil?
-			if !wp.whitepaper.nil?
+			if !wp.whitepaper.url.nil?
 				io = open('https://s3.amazonaws.com/coindexter/' + wp.whitepaper.current_path)
 				reader = PDF::Reader.new(io)
 				wp.fulltext = ""
@@ -17,15 +17,23 @@ class SaveTextsFromPdfsWorker
 				end
 			elsif !url.nil? 
 				if URI.parse(url).host == 'github.com'
+					puts 'host is github'
 					if url.include? 'Documentation'
+						puts 'host includes Documentation'
 						raw_url = url.gsub('https://github.com/','https://raw.githubusercontent.com/')
+						puts 'raw url is #{raw_url}'
 						raw_url = raw_url.gsub('blob/master','master')
+						puts 'raw url is now #{raw_url}'
 						doc = Nokogiri::HTML(open(raw_url))
+						puts 'doc is #{doc}'
 						text = doc.children[1].children[0].children[0].children[0].text
+						puts 'text is #{text}'
 						wp.fulltext = text
 					elsif url.include? 'wiki'
 						doc = Nokogiri::HTML(open(url))
+						puts 'doc 2 is #{doc}'
 						text = doc.css('div#wiki-body').text
+						puts 'text 2 is #{text}'
 						wp.fulltext = text
 					end
 				end
