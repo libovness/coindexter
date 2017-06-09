d76136952fe3ed727656dd73e6a77f9a9600d302
libovness <jonathan.libov@gmail.com>
2017-06-08 22:30:19 -0400

    fixed subject in digest email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,11 @@ class UserMailer < ActionMailer::Base
     @coin_logs = coin_logs
     @coins_following = coins_following
     update_sum = network_logs.count + coin_logs.count == 1 ? "1 update" : "#{network_logs.count + coin_logs.count} updates" 
-    mail(to: @user.email, subject: "Coindexter Daily Digest: #{update_sum}")
-    puts "mail sent to #{@user.email}"
+    if network_logs.count + coin_logs.count == 0
+      mail(to: @user.email, subject: "Coindexter Daily Digest").deliver
+    else   
+      mail(to: @user.email, subject: "Coindexter Daily Digest: #{update_sum}").deliver
+    end
   end
 
   def confirmation_instructions(user, confirmation_instructions, opts={})
