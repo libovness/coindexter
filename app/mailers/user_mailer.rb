d8bcf5481004d3ed495a18902b69f45a412db093
libovness <jonathan.libov@gmail.com>
2017-06-12 07:54:26 -0400

    improvements to how things are mailed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,28 +9,28 @@ class UserMailer < ActionMailer::Base
     @coins_following = coins_following
     update_sum = network_logs.count + coin_logs.count == 1 ? "1 update" : "#{network_logs.count + coin_logs.count} updates" 
     if network_logs.count + coin_logs.count == 0
-      mail(to: @user.email, subject: "Coindexter Daily Digest").deliver
+      mail(to: @user.email, subject: "Coindexter Daily Digest")
     else   
-      mail(to: @user.email, subject: "Coindexter Daily Digest: #{update_sum}").deliver
+      mail(to: @user.email, subject: "Coindexter Daily Digest: #{update_sum}")
     end
   end
 
   def daily_digest_new(user, all_network_logs)
     @user = user
     @all_network_logs = all_network_logs
-    mail(to: @user.email, subject: "Coindexter Daily Digest").deliver
+    mail(to: @user.email, subject: "Coindexter Daily Digest")
   end
 
   def confirmation_instructions(user, confirmation_instructions, opts={})
     @user = user
     @confirmation_instructions = confirmation_instructions
-    mail(to: @user.email, subject: "Confirm your email address").deliver
+    mail(to: @user.email, subject: "Confirm your email address").deliver_later
   end
 
   def reset_password_instructions(user, reset_password_token, opts={})
     @user = user
     @reset_password_token = reset_password_token
-    mail(to:@user.email, subject: "Reset your password").deliver
+    mail(to:@user.email, subject: "Reset your password").deliver_later
   end
 
 end
