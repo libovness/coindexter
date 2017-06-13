b61539c55479a9fa24a03aa74d7ba7627aac54af
libovness <jonathan.libov@gmail.com>
2017-06-12 23:37:17 -0400

    updated daily digest

--- app/mailers/confirmation_instructions.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  
- default from: "Coindexter <jonathan@coindexter.io>"
-
-  def confirmation_instructions(user, confirmation_instructions)
-  	mail(to: @user.email, subject: "Confirm you remail address")
-  end
-
-end
--- app/workers/daily_digest_worker.rb
+++ app/workers/daily_digest_worker.rb
@@ -9,43 +9,42 @@ class DailyDigestWorker
 
   def perform
 
-      all_network_logs = []
+    all_network_logs = []
 
-      Network.find_each do |network| 
-        network_logs = NetworkService.new
-        fetched_network_logs = network_logs.get_logs(network, "Network",nil,nil,1).reverse
-        logs = {network.name => []}
-        unless fetched_network_logs.empty?
-          logs[network.name] << {:network_logs => fetched_network_logs}
-        end
-
+    Network.find_each do |network| 
+      network_logs = NetworkService.new
+      fetched_network_logs = network_logs.get_logs(network, "Network",nil,nil,1).reverse
+      logs = { network: network, logs: fetched_network_logs, coins: [] }
+      if !network.coins.nil?
         network.coins.each do |coin|
           coin_logs = NetworkService.new
           fetched_coin_logs = coin_logs.get_logs(coin, "Coin",nil,nil,1).reverse
-          network_coin_logs = {coin.name => {}}
-          unless fetched_coin_logs.empty? 
-            network_coin_logs[coin.name] = {:logs => fetched_coin_logs, :price => coin.price}
+          unless fetched_coin_logs.empty? && coin.price.nil?
+            network_coin_logs =  { :coin => coin, :logs => fetched_coin_logs }
           end 
-          logs[network.name] << {:network_coin_logs => network_coin_logs}
+          logs[:coins] << network_coin_logs
         end
-          
-        all_network_logs << logs
+      end 
 
+
+
+      all_network_logs << logs unless logs[:logs].empty? && (logs[:coins].nil? or logs[:coins] == [nil] or logs[:coins].empty?)
+
+    end
+
+    networks_following = []
+
+    user = User.first
+
+    user.all_follows.each do |follow|
+      if follow.followable_type == "Network"
+        networks_following << Network.find(follow.followable_id).name
       end
+    end
 
-      networks_following = []
-
-      user = User.first
-
-      user.all_follows.each do |follow|
-        if follow.followable_type == "Network"
-          networks_following << Network.find(follow.followable_id).name
-        end
-      end
-
-      all_network_logs = all_network_logs.select do |network|
-        networks_following.index network.keys.first
-      end
+    all_network_logs = all_network_logs.select do |network|
+      networks_following.index network[:network].name
+    end
 
     UserMailer.daily_digest_new(user, all_network_logs).deliver_later
   
