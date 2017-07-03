0c745510db7bea0e6cc7d9b616219a07b79c5a7c
libovness <jonathan.libov@gmail.com>
2017-07-03 19:07:16 -0400

    improved logs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}, sign_up: 'signup', sign_in: 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'networks#index'
+  root 'users#index'
   
   resources :categories
   
