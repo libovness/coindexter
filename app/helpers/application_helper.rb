0a96474bf2a677bb89dbcd523cbfcc38e36085c4
libovness <jonathan.libov@gmail.com>
2017-07-11 23:15:02 -0400

    removed http from links on show pages and logs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,11 @@ module ApplicationHelper
     end
   end  
 
+  def get_host_without_www(url)
+    uri = URI.parse(url)
+    uri = URI.parse("http://#{url}") if uri.scheme.nil?
+    host = uri.host.downcase
+    host.start_with?('www.') ? host[4..-1] : host
+  end
+
 end
