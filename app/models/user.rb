47e5aecdd7ffb8fbf6bca5ed3714441e43c25624
libovness <jonathan.libov@gmail.com>
2017-07-20 02:16:40 +0300

    maybe fixed manual login 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ApplicationRecord
   end
 
   def password_required?
-    new_record? && provider.blank? ? true : false
+    super && blank?
   end
 
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
