038dfa0daed9d4aa15acc5d778acad70e503bd76
libovness <jonathan.libov@gmail.com>
2017-07-20 18:19:35 +0300

    fixed facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,2 @@
 OmniAuth.config.full_host = Rails.env.production? ? 'https://coindexter.io' : 'http://localhost:3000'
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
-   client_options: {
-      site: 'https://graph.facebook.com/v2.10', 
-      authorize_url: "https://www.facebook.com/v2.10/dialog/oauth"
-    }
-end
