c9c9fbc57e18fbd7a69d6955a21ec4d1bb38d6dd
libovness <jonathan.libov@gmail.com>
2017-08-26 15:11:59 -0400

    attempted fix for login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -56,9 +56,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
     else
-      puts 'ENV["omniauth.auth"] is #{ENV["omniauth.auth"]}'
-      puts 'env["omniauth.auth"] is #{env["omniauth.auth"]}'
-      session["devise.#{provider}_data"] = env["omniauth.auth"].except("extra")
+      puts "ENV['omniauth.auth'] is #{ENV['omniauth.auth']}"
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
       sign_in_and_redirect @user
     end
   end
